f875e8b9f27abd32cec1ab8c0b512ca91796f173
raine2703 <118910194+raine2703@users.noreply.github.com>
2022-12-09 12:25:53 +0200

    Update Scipt.ps1

--- Scipt.ps1
+++ Scipt.ps1
@@ -4,10 +4,10 @@
 #--------------------------------------------------------------------------------------------
 #Check version
 git --version
---------------------------------------------------------------------------------------------
+#--------------------------------------------------------------------------------------------
 #Turn any folder into a repo which creates a sub .git folder
 git init
---------------------------------------------------------------------------------------------
+#--------------------------------------------------------------------------------------------
 #Set initial git configuration
 #Can replace --global with --local to set values for a specific repo if required
 git config --global --list    #care about username and email
@@ -19,8 +19,8 @@ git config --list --show-origin      #see where coming from :q
 git config --list
 
 #Many other settings https://git-scm.com/book/en/v2/Getting-Started-First-Time-Git-Setup
-git config --global init.defaultBranch main #this is very common to use instead of master
--------------------------------------------------------------------------------------------
+git config --global init.defaultBranch main #this is very common to use instead of master#
+#-------------------------------------------------------------------------------------------
 #push to existing github, first have to add remote origin, then pull/push.
 git remote add origin https://github.com/raine2703/test3
 
